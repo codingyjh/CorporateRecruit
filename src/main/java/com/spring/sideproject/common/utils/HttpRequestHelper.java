7ef24d64f37e8e06ff5289e938342535ec5c7f95
codingyjh <codingyjh@gmail.com>
2021-11-06 21:34:31 +0900

    2021.11.06 : 공통 HttpRequestHelper 유틸 모듈 추가.

---
+++ src/main/java/com/spring/sideproject/common/utils/HttpRequestHelper.java
@@ -0,0 +1,76 @@
+package com.spring.sideproject.common.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+public class HttpRequestHelper {
+
+	public static final String REDIRECT = "redirect";
+
+	public enum Platform {
+		
+		PC("", "pc"), SMART("s_", "smart");
+
+		private String jspPrefix;
+		private String folderPath;
+		
+		private Platform(String jspPrefix, String folderPath) {
+			this.jspPrefix = jspPrefix;
+			this.folderPath = folderPath;
+		}
+
+		public String getJspPrefix() {
+			return jspPrefix;
+		}
+
+		public void setJspPrefix(String jspPrefix) {
+			this.jspPrefix = jspPrefix;
+		}
+
+		public String getFolderPath() {
+			return folderPath;
+		}
+
+		public void setFolderPath(String folderPath) {
+			this.folderPath = folderPath;
+		}
+				
+	}
+	
+	public static Platform getPlatformByUri(String uri) {
+		if ( uri.contains("/s_")) {
+			return Platform.SMART;
+		} else {
+			return Platform.PC;
+		}
+	}
+	
+	public static String getUri() {
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+				.getRequest();
+		String uri = request.getRequestURI();
+		
+		return uri.toString();
+	}
+	
+	public static String getJspPath() {
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+		
+		Platform platform = getPlatformByUri(request.getRequestURI());
+		
+		String uri = request.getRequestURI();
+//		String jsp = uri.substring(0, uri.lastIndexOf("."));
+		String jsp = uri;
+		
+		StringBuffer fullPath = new StringBuffer();
+		
+		String folder = platform.getFolderPath();
+		fullPath.append(folder);
+		fullPath.append(jsp);
+		
+		return fullPath.toString();		
+	}	
+	
+}

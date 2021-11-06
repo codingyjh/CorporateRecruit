1b58bdec18714b6b849ee7541a31ead03fca3f11
codingyjh <codingyjh@gmail.com>
2021-11-06 21:51:54 +0900

    2021.11.06 : application.xml - 정적자원, tiles 설정 및 WEB-INF 구성 완료.

---
+++ src/main/webapp/WEB-INF/static/js/common/common.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+jQuery.extend({
+	CommonObj : function() {
+		
+		var that = this;
+		var $form = null;
+		var $body = null;
+		
+		this.onCreate = function() {};		
+		this.eventInit = function() {};
+		
+		this.init = function() {
+			that.$body = $('body');
+			this.eventInit();
+			this.onCreate();
+		};
+	}
+});
+
+$(document).ready(function() {
+	var CommonObj = new $.CommonObj();
+});

31224fba10a2c343886ef12d089c3ad50c6b2b7b
codingyjh <codingyjh@gmail.com>
2021-11-07 22:51:17 +0900

    2021.11.07 : recruitMemberLogin.js 소스 리팩토링 완료.

--- src/main/webapp/WEB-INF/static/js/recruitMember/recruitMemberLogin.js
+++ src/main/webapp/WEB-INF/static/js/recruitMember/recruitMemberLogin.js
@@ -2,59 +2,101 @@
  * 
  */
 
-$(document).ready(function() {
-
-	$("#email").keyup(function() {			
-		emailCheck();
-	});
-	
-	$("#password").keyup(function() {
-		$("#passwordMessage").hide();				
-	});
-	
-	function emailCheck() {
+var controller = $.extend(new $.CommonObj(), {
+	eventInit:function() {
+		
+	},	
+	onCreate:function() {
+		
+		$("#email").keyup(function() {			
+			controller.emailCheck();
+		});
+		
+		$("#password").keyup(function() {
+			$("#passwordMessage").hide();				
+		});
+		
+		$("#loginBtn").click(function() {
+			
+			if ( controller.validationCheck() ) {
+				return;
+			}
+			else {
+				controller.autoClosingAlert("#successMessage", 2000);
+				$("#loginForm").attr({
+					method:"post", 				     	
+			     	action:"/Recruit/recruitMemberLogin.do"
+				}).submit();	
+			}
+		});
+		
+		$("passwordFindPopup").click(function() {
+			window.open("http://localhost:8080/recruitMember/passwordFindPopup")
+		});
+		
+	}, emailCheck : function() {
+		
 		var email = $("#email").val();
 		var reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		
 		if ( reg.test(email) ) {
 			$("#emailMessage").hide();
-			successState("#email");
+			controller.successState("#email");
 			return false;
-		} else {
+		}
+		else {
 			$("#emailMessage").text("올바른 이메일 형식이 아닙니다. 다시 입력해 주세요.").show();
-			errorState("#email");
+			controller.errorState("#email");
 			return true;
 		}
-	}
+		
+	}, successState : function(sel) {
 	
-	function successState(sel) {
+		$(sel).closest(".form-group")
+  	   	  .removeClass("has-error")
+  	   	  .find(".form-control-feedback")
+  	   	  .removeClass("glyphicon-ok")
+  	   	  .removeClass("glyphicon-remove")
+  	   	  .show();
+		
+	}, errorState : function(sel) {
 		
 		$(sel).closest(".form-group")
-  	  	   	  .removeClass("has-error")
-  	  	   	  .find(".form-control-feedback")
-  	  	   	  .removeClass("glyphicon-ok")
-  	  	   	  .removeClass("glyphicon-remove")
-  	  	   	  .show();
-	}
-	
-	function errorState(sel) {
+		  .removeClass("has-success")
+		  .addClass("has-error")
+		  .find(".form-control-feedback")
+		  .removeClass("glyphicon-ok")
+		  .addClass("glyphicon-remove")
+		  .show();
 		
-		$(sel).closest(".form-group")
-			  .removeClass("has-success")
-			  .addClass("has-error")
-			  .find(".form-control-feedback")
-			  .removeClass("glyphicon-ok")
-			  .addClass("glyphicon-remove")
-			  .show();
-	}
-	
-	function autoClosingAlert(selector, delay) {
+	}, autoClosingAlert : function(selector, delay) {
+		
 		var alert = $(selector).alert();
 		alert.show();
 		window.setTimeout(function() { alert.hide() }, delay);	
-	}
-	
-	function emptyCheck() {
+		
+	}, validationCheck : function() {
+		
+		controller.emptyCheck();
+		
+		if ( emailEmpty || passwordEmpty ) {
+			if ( emailEmpty ) {
+				$("#emailMessage").text("이메일을 입력해주세요.").show();
+				controller.errorState("#email");
+			}
+			if ( passwordEmpty ) {
+				$("#passwordMessage").text("비밀번호를 입력해주세요.").show();
+				controller.errorState("#password");
+			}
+			return true;
+		} else {
+			if ( controller.emailCheck() == true ) {
+				return true;
+			}
+			return false;
+		}
+		
+	}, emptyCheck : function() {
 		
 		emailEmpty = false;
 		passwordEmpty = false;
@@ -66,44 +108,8 @@ $(document).ready(function() {
 			passwordEmpty = true;
 		}
 	}
-	
-	function validationCheck() {
-		
-		emptyCheck();
-		
-		if ( emailEmpty || passwordEmpty ) {
-			if ( emailEmpty ) {
-				$("#emailMessage").text("이메일을 입력해주세요.").show();
-				errorState("#email");
-			}
-			if ( passwordEmpty ) {
-				$("#passwordMessage").text("비밀번호를 입력해주세요.").show();
-				errorState("#password");
-			}
-			return true;
-		} else {
-				if ( emailCheck() == true ) {
-					return true;
-				}
-				
-				return false;
-		}
-	}
-	
-	$("#loginBtn").click(function() {
-		
-		if ( validationCheck() ) {
-			return;
-		} else {
-			autoClosingAlert("#successMessage", 2000);
-			$("#loginForm").attr({
-				method:"post", 				     	
-		     	action:"/recruitMember/recruitMemberLogin"
-			}).submit();	
-		}
-	});	
+});
 
-	$("passwordFindPopup").click(function() {
-		window.open("https://localhost:8080/recruitMember/passwordFindPopup");
-	});	
-}); 
+$(document).ready(function() {
+	controller.init();
+});

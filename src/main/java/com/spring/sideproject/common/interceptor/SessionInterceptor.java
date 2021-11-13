b40003b24428a6b81f269e94c5ddf11f8e930470
codingyjh <codingyjh@gmail.com>
2021-11-13 20:33:35 +0900

    2021.11.13 : Interceptor 세팅 및 기능 추가.

---
+++ src/main/java/com/spring/sideproject/common/interceptor/SessionInterceptor.java
@@ -0,0 +1,27 @@
+package com.spring.sideproject.common.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.spring.sideproject.common.session.Session;
+import com.spring.sideproject.recruitmember.vo.RecruitMemberVo;
+
+public class SessionInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		HttpSession session = request.getSession();
+		RecruitMemberVo recruitMemberVo = (RecruitMemberVo) session.getAttribute(Session.USER);
+		
+		if ( recruitMemberVo == null ) {
+			response.sendRedirect("/recruitMember/recruitMemberLogin");
+			return false;
+		}
+		return true;		
+	}
+}

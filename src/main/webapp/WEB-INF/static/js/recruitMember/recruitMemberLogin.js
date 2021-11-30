1c66902012d26e170c02b7f19e52179ef66fced2
codingyjh <codingyjh@gmail.com>
2021-11-30 23:59:27 +0900

    2021.11.30 : 로그인 시 로딩중 기능 추가.

--- src/main/webapp/WEB-INF/static/js/recruitMember/recruitMemberLogin.js
+++ src/main/webapp/WEB-INF/static/js/recruitMember/recruitMemberLogin.js
@@ -20,8 +20,10 @@ var controller = $.extend(new $.CommonObj(), {
 			
 			if ( controller.validationCheckFunc() ) {
 				return;
-			}
-			else {
+				
+			} else {
+				controller.LoadingWithMaskFunc();
+				setTimeout("controller.closeLoadingWithMaskFunc()", 10000);
 				//controller.autoClosingAlert("#successMessage", 2000);
 				$("#loginForm").attr({
 					method:"post", 				     	
@@ -43,8 +45,8 @@ var controller = $.extend(new $.CommonObj(), {
 			$("#emailMessage").hide();
 			controller.successState("#email");
 			return false;
-		}
-		else {
+			
+		} else {
 			$("#emailMessage").text("올바른 이메일 형식이 아닙니다. 다시 입력해 주세요.").show();
 			controller.errorState("#email");
 			return true;
@@ -80,19 +82,25 @@ var controller = $.extend(new $.CommonObj(), {
 		controller.emptyCheckFunc();
 		
 		if ( emailEmpty || passwordEmpty ) {
+			
 			if ( emailEmpty ) {
 				$("#emailMessage").text("이메일을 입력해주세요.").show();
 				controller.errorState("#email");
 			}
+			
 			if ( passwordEmpty ) {
 				$("#passwordMessage").text("비밀번호를 입력해주세요.").show();
 				controller.errorState("#password");
 			}
+			
 			return true;
+			
 		} else {
+			
 			if ( controller.emailCheckFunc() == true ) {
 				return true;
 			}
+			
 			return false;
 		}
 		
@@ -104,9 +112,50 @@ var controller = $.extend(new $.CommonObj(), {
 		if ( $("#email").val() == "" ) {
 			emailEmpty = true;
 		}
+		
 		if ( $("#password").val() == "" ) {
 			passwordEmpty = true;
 		}
+		
+	}, LoadingWithMaskFunc : function() {
+		
+	    //화면의 높이와 너비를 구합니다.
+	    var maskHeight = $(document).height();
+	    var maskWidth  = window.document.body.clientWidth;
+	    
+	    console.log('maskHeight : ' + maskHeight);
+	    console.log('maskWidth : ' + maskWidth);
+	    
+	     
+	    //화면에 출력할 마스크를 설정해줍니다.
+	    var mask       = "<div id='mask' style='position:absolute; z-index:9000; background-color:#000000; display:none; left:0; top:0;'></div>";
+	    var loadingImg = '';
+	      
+	    loadingImg += "<div id='loadingImg'>";
+	    loadingImg += " <img src='/images/common/LoadingImg.gif' style='position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); display: block; '/>";
+	    loadingImg += "</div>"; 
+	  
+	    //화면에 레이어 추가
+	    $('body')
+	        .append(mask)
+	        .append(loadingImg)
+	        
+	    //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채웁니다.
+	    $('#mask').css({
+	            'width' : maskWidth
+	            ,'height': maskHeight
+	            ,'opacity' :'0.3'
+	    });
+	  
+	    //마스크 표시
+	    $('#mask').show();  
+	  
+	    //로딩중 이미지 표시
+	    $('#loadingImg').show();
+	    
+	}, closeLoadingWithMaskFunc : function() {
+	    $('#mask, #loadingImg').hide();
+	    $('#mask, #loadingImg').remove(); 
 	}
 });
 

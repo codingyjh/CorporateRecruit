0f7f424ec73cb81d7078f2a4488041c4cee9d02c
codingyjh <codingyjh@gmail.com>
2021-12-01 23:47:18 +0900

    2021.12.01 : 채용문의 메일 송신 시 로딩중 로직 추가.

--- src/main/webapp/WEB-INF/static/js/recruitmentQuestion/recruitmentQuestion.js
+++ src/main/webapp/WEB-INF/static/js/recruitmentQuestion/recruitmentQuestion.js
@@ -21,7 +21,9 @@ var controller = $.extend(new $.CommonObj(), {
 					return false;
 				}
 					
-				controller.autoClosingAlert("#successMessage", 13000);
+				controller.LoadingWithMaskFunc();
+				setTimeout("controller.closeLoadingWithMaskFunc()", 20000);
+				//controller.autoClosingAlert("#successMessage", 2000);
 				$("#recruitmentQuestionEmailForm").attr({
 					method:"post", 
 			     	action:"/recruitmentQuestion/recruitmentQuestion.do"					
@@ -34,6 +36,46 @@ var controller = $.extend(new $.CommonObj(), {
 		var alert = $(selector).alert();
 		alert.show();
 		window.setTimeout(function() { alert.hide() }, delay);
+		
+	}, LoadingWithMaskFunc : function() {
+		
+	    //화면의 높이와 너비를 구합니다.
+	    var maskHeight = $(document).height();
+	    var maskWidth  = window.document.body.clientWidth;
+	    
+	    console.log('maskHeight : ' + maskHeight);
+	    console.log('maskWidth : ' + maskWidth);
+	    
+	     
+	    //화면에 출력할 마스크를 설정해줍니다.
+	    var mask       = "<div id='mask' style='position:absolute; z-index:9000; background-color:#000000; display:none; left:0; top:0;'></div>";
+	    var loadingImg = '';
+	      
+	    loadingImg += "<div id='loadingImg'>";
+	    loadingImg += " <img src='/images/common/LoadingImg.gif' style='position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); display: block; '/>";
+	    loadingImg += "</div>"; 
+	  
+	    //화면에 레이어 추가
+	    $('body')
+	        .append(mask)
+	        .append(loadingImg)
+	        
+	    //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채웁니다.
+	    $('#mask').css({
+	            'width' : maskWidth
+	            ,'height': maskHeight
+	            ,'opacity' :'0.3'
+	    });
+	  
+	    //마스크 표시
+	    $('#mask').show();  
+	  
+	    //로딩중 이미지 표시
+	    $('#loadingImg').show();
+	    
+	}, closeLoadingWithMaskFunc : function() {
+	    $('#mask, #loadingImg').hide();
+	    $('#mask, #loadingImg').remove(); 
 	}
 });
 
